addb35cc1f537057257022ee39f13a3cc3b9ac03
vyash5075 <vyash5075@gmail.com>
2020-01-12 20:36:51 +0530

    pattern created

---
+++ RightPascalTriangle.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+/**
+ 
+  *
+  **
+  ***
+  ****
+  *****
+  ****
+  ***
+  **
+  *
+ */
+public class RightPascalTriangle {
+  public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+      int rows=sc.nextInt();
+      int columns=sc.nextInt();
+     
+      for(int i=1;i<=rows;i++)
+      {
+          for(int j=1;j<=i;j++)
+          {
+              System.out.print(" * ");
+          }
+          System.out.println("");
+      }
+      for(int i=1;i<=rows;i++)
+      {
+          for(int j=columns-1;j>=i;j--)
+          {
+              System.out.print(" * ");
+          }
+          System.out.println(" ");
+      }
+    }  
+}
