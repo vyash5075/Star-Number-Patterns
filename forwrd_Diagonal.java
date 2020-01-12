50f6b93e2b83cb4d185f2bd01c868ac098394c65
vyash5075 <vyash5075@gmail.com>
2020-01-12 20:42:35 +0530

    pattern created

---
+++ forwrd_Diagonal.java
@@ -0,0 +1,27 @@
+import java.util.*;
+/**
+      *
+       *
+        *
+         *
+          *
+           * 
+ */
+public class forwrd_Diagonal {
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter the no. of rows");
+        int rows=sc.nextInt();
+        for(int i=1;i<=rows;i++)
+        {
+            for(int j=1;j<=i;j++)
+            {
+                System.out.print(" ");
+            }
+            System.out.print("*");
+            System.out.println("");
+        }
+    }
+    
+}
