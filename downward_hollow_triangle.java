aa094a036272ee817deb05f71d796d4700c7bdca
vyash5075 <vyash5075@gmail.com>
2020-01-12 20:40:42 +0530

    pattern created

---
+++ downward_hollow_triangle.java
@@ -0,0 +1,42 @@
+import java.util.*;
+/**
+ *
+ * @author yash verma
+ * 
+ * 
+ * 
+ * 
+ *      *********
+         *     *
+          *   *
+           * *
+            *
+ */
+public class downward_hollow_triangle {
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter the no. of rows");
+        int rows=sc.nextInt();
+        for(int i=rows;i>=1;i--)
+        {
+            for(int j=i;j<rows;j++)
+            {
+                System.out.print(" ");
+            }
+            
+            for(int k=1;k<=(2*i-1);k++)
+            {
+                if(i==rows||k==1||k==(2*i-1))
+                {
+                   System.out.print("*");
+                }
+                else{
+                    System.out.print(" "); 
+                }
+            }
+           
+            System.out.println("");
+        }
+    }
+}
