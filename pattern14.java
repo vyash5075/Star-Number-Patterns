78eed90945b66270b88dce1a706fe467fba8b9b0
vyash5075 <vyash5075@gmail.com>
2020-01-13 20:08:15 +0530

    pattern created

---
+++ pattern14.java
@@ -0,0 +1,43 @@
+import java.util.*;
+
+/**
+ 
+ 
+  
+ *     * 
+  *   * 
+   * * 
+    *
+    
+    
+    
+ */
+public class pattern14 {
+  public static void main(String args[])
+  {
+    Scanner sc=new Scanner(System.in);
+    System.out.println("enter the no. of rows");
+    int rows=sc.nextInt();
+    int space=1;
+    for(int j = 1; j<= rows- 1; j++)
+    {
+        for(int i = 1; i<= j; i++)
+        {
+            System.out.print(" ");
+            
+        }
+       for (int k = 1; k<= 2 *(rows - j) - 1; k++)
+        {
+            if(k>1&& k< 2 *(rows - j)-1)
+            {
+                System.out.print(" ");
+            }
+            else{
+                  System.out.print("*");
+            }
+      
+        }
+        System.out.println(" ");
+    }   
+  }
+}
